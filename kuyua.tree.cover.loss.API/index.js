95e5e3ddc50ce33de8ea1c6e34c69cfc186d5c59
ahmed-kareem <ahmed.kareem211@yahoo.com>
2024-05-22 16:24:31 +0300

    work with mapbox

--- kuyua.tree.cover.loss.API/index.js
+++ kuyua.tree.cover.loss.API/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const AuthenticationMW = require('./middlewares/AuthMiddleware');
 const ErrorHandlingMW = require('./middlewares/ErrorHandlingMiddleware');
 
 const app = express()
+app.use(cors());
 const port = 3000
 
 app.use(AuthenticationMW);
